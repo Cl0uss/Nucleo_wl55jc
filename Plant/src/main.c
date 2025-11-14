0b412e77d7c19bbf233759de0783444d3857d413
Cl0us <kasradze.niko@gmail.com>
2025-11-14 19:29:50 +0100

    Initial commit

---
+++ Plant/src/main.c
@@ -0,0 +1,25 @@
+#include <zephyr/kernel.h>
+#include <zephyr/drivers/gpio.h>
+#include <zephyr/device.h>
+#include <zephyr/drivers/i2c.h>
+#include <zephyr/sys/printk.h>
+#include <zephyr/kernel/stats.h>
+#include <zephyr/arch/arch_interface.h>
+
+
+void rgbInit(){
+    return;
+}
+
+void accelerometerInit(){
+    return;
+}
+
+void tempInit(){
+    return;
+}
+
+
+void main(void) {
+
+}
