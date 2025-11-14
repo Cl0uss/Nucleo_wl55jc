6e1f4ddaad7232a1c3f53a137d51a1a4a41cbe8a
Cl0us <kasradze.niko@gmail.com>
2025-11-14 22:59:54 +0100

    rgb proccess

---
+++ Plant/src/connector.h
@@ -0,0 +1,1 @@
+#define rgbAddr 0x29
--- Plant/src/main.c
+++ Plant/src/main.c
@@ -5,10 +5,26 @@
 #include <zephyr/sys/printk.h>
 #include <zephyr/kernel/stats.h>
 #include <zephyr/arch/arch_interface.h>
+#include "connector.h"
 
+const struct device *i2c;
+uint8_t registers[2];
+
+enum Mode {TEST, NORMAL, ADVANCED};
+
+
+
+void registersInput(uint8_t first, uint8_t second) {
+    registers[0] = first;
+    registers[1] = second;
+}
 
 void rgbInit(){
-    return;
+    registersInput(0x80 | 0x00, 0x01);
+    i2c_write(i2c,registers,sizeof(registers),rgbAddr);
+    k_msleep(3);
+    registersInput(0x80 | 0x00, 0x3);
+    i2c_write(i2c,registers,sizeof(registers),rgbAddr);
 }
 
 void accelerometerInit(){
@@ -20,6 +36,27 @@ void tempInit(){
 }
 
 
+void gpsInit(){
+    return;
+}
+
+
+void measures(){
+    return;
+}
+
+K_THREAD_DEFINE (measureThread,4096,measures,NULL,NULL,NULL,0,0,0);
+
 void main(void) {
+    
+    i2c = DEVICE_DT_GET(DT_NODELABEL(i2c2));
+    if (!device_is_ready(i2c)) {
+        printk("i2c2 not ready");
+    }
+
+    while (true){
+
+    }
+
 
 }
