cb162e5fca1eba3cf149fa5cec533107f065db7e
Cl0us <kasradze.niko@gmail.com>
2025-11-29 19:16:14 +0100

    Finished

--- Plant/measures/soil.c
+++ Plant/measures/soil.c
@@ -1,10 +1,10 @@
 #include "../src/connector.h"
 
-// on earth (min) - 900   wet water (max) - 3600
+//  (min) - 200   wet water (max) - 3600
 
 void soilMeasure(void){
 
     adc_read(adc, &soilSeq);
     //printk("soilRawVal = %d\n",soilRawVal);
-    soilValue = (soilRawVal - 900.0f) * 100.0f / (3600.0f - 900.0f);
+    soilValue = (soilRawVal - 200.0f) * 100.0f / (3600.0f - 200.0f);
 }
